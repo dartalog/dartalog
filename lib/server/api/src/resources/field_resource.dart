66d31a037ba81bf62cecb9577a4d559fe6121d5b
Matthew Barbour <sanmadjack@gmail.com>
2015-05-19 08:29:04 -0500

    a

--- lib/server/api/src/resources/field_resource.dart
+++ lib/server/api/src/resources/field_resource.dart
@@ -9,4 +9,16 @@ class FieldResource {
       return output;
   }
 
+  @ApiMethod(method: 'POST', path: 'fields/')
+  Future<String> create(Field field) async {
+    String output = await model.write(field);
+    return output;
+  }
+
+  @ApiMethod(method: 'PUT', path: 'fields/{uuid}/')
+  Future<String> update(String uuid, Field field) async {
+    String output = await model.write(field,uuid);
+    return output;
+  }
+
 }
